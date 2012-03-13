6bd7432d996789b3c35c9e92e2ef62a768286913
jdalsem <jdalsem@coldtrick.com>
2012-03-13 10:18:00 +0100

    added: german translation

---
+++ languages/de.php
@@ -0,0 +1,22 @@
+<?php
+$language = array (
+  'image_captcha' => 'Bilder CAPTCHA',
+  'image_captcha:settings:icons' => 'Wähle welche Bilder du für das CAPTCHA benutzen möchtest',
+  'image_captcha:settings:icons:general' => 'Allgemein',
+  'image_captcha:settings:icons:fruit' => 'Früchte',
+  'image_captcha:icon_types:fruit:apple' => 'den Apfel',
+  'image_captcha:icon_types:fruit:cherry' => 'die Kirsche',
+  'image_captcha:icon_types:fruit:lemon' => 'die Zitrone',
+  'image_captcha:icon_types:fruit:pear' => 'die Erbse',
+  'image_captcha:icon_types:fruit:strawberry' => 'die Erdbeere',
+  'image_captcha:icon_types:general:folder' => 'den Ordner',
+  'image_captcha:icon_types:general:house' => 'das Haus',
+  'image_captcha:icon_types:general:lock' => 'das Schloß',
+  'image_captcha:icon_types:general:man' => 'den Mann',
+  'image_captcha:icon_types:general:monitor' => 'den Bildschirm',
+  'image_captcha:icon_types:general:rss' => 'den RSS-Feed',
+  'image_captcha:icon_types:general:woman' => 'die Frau',
+  'image_captcha:label' => 'Wenn Du ein Mensch bist, bitte wähle <span style="text-decoration: underline;">%s</span>.',
+  'image_captcha:verify:fail' => 'Entschuldigung, aber das ausgewählte Bild war falsch.',
+);
+add_translation("de", $language);
