2ad509e5c717b29eccd7e59b9ba3cf721a77c6d4
jdalsem <jdalsem@coldtrick.com>
2015-01-15 15:04:36 +0100

    fixed: correct indentation

--- start.php
+++ start.php
@@ -1,63 +1,68 @@
 <?php 
 
-	function image_captcha_init(){
-		
-		// Register a function that provides some default override actions
-		elgg_register_plugin_hook_handler('actionlist', 'captcha', 'image_captcha_actionlist_hook');
-		// Register actions to intercept
-		$actions = elgg_trigger_plugin_hook('actionlist', 'captcha', null, array());
-		
- 		elgg_define_js('s3capcha', array(
-			'src' => '/mod/image_captcha/vendors/s3capcha/s3capcha.js',
-			'exports' => 's3capcha',
-		));
-		
-		if (($actions) && (is_array($actions))) {
-			foreach ($actions as $action){
-				elgg_register_plugin_hook_handler("action", $action, "image_captcha_verify_action_hook");
-			}
-		}
-	}
+/**
+ * Initializes the captcha plugin
+ * 
+ * @return void
+ */
+function image_captcha_init(){
+	elgg_define_js('s3capcha', array(
+		'src' => '/mod/image_captcha/vendors/s3capcha/s3capcha.js',
+		'exports' => 's3capcha',
+	));
 	
-	/**
-	 * Listen to the action plugin hook and check the captcha.
-	 *
-	 * @param unknown_type $hook
-	 * @param unknown_type $entity_type
-	 * @param unknown_type $returnvalue
-	 * @param unknown_type $params
-	 */
-	function image_captcha_verify_action_hook($hook, $entity_type, $returnvalue, $params) {
-		$token = get_input('image_captcha');
-		if (($token) && ($token == $_SESSION["image_captcha"])){
-			return true;
-		} else {
-			register_error(elgg_echo('image_captcha:verify:fail'));
-		}
-		// forward to referrer or else action code sends to front page
-		forward(REFERER);
-	}
+	// Register a function that provides some default override actions
+	elgg_register_plugin_hook_handler('actionlist', 'captcha', 'image_captcha_actionlist_hook');
 	
-	/**
-	 * This function returns an array of actions the captcha will expect a captcha for, other plugins may
-	 * add their own to this list thereby extending the use.
-	 *
-	 * @param unknown_type $hook
-	 * @param unknown_type $entity_type
-	 * @param unknown_type $returnvalue
-	 * @param unknown_type $params
-	 */
-	function image_captcha_actionlist_hook($hook, $entity_type, $returnvalue, $params){
-		if (!is_array($returnvalue)){
-			$returnvalue = array();
+	// Register actions to intercept
+	$actions = elgg_trigger_plugin_hook('actionlist', 'captcha', null, array());
+
+	if (($actions) && (is_array($actions))) {
+		foreach ($actions as $action){
+			elgg_register_plugin_hook_handler("action", $action, "image_captcha_verify_action_hook");
 		}
-			
-		$returnvalue[] = 'register';
-		$returnvalue[] = 'user/requestnewpassword';
-			
-		return $returnvalue;
+	}
+}
+	
+/**
+ * Listen to the action plugin hook and check the captcha.
+ *
+ * @param unknown_type $hook
+ * @param unknown_type $entity_type
+ * @param unknown_type $returnvalue
+ * @param unknown_type $params
+ */
+function image_captcha_verify_action_hook($hook, $entity_type, $returnvalue, $params) {
+	$token = get_input('image_captcha');
+	if (($token) && ($token == $_SESSION["image_captcha"])){
+		return true;
+	} else {
+		register_error(elgg_echo('image_captcha:verify:fail'));
+	}
+	// forward to referrer or else action code sends to front page
+	forward(REFERER);
+}
+
+/**
+ * This function returns an array of actions the captcha will expect a captcha for, other plugins may
+ * add their own to this list thereby extending the use.
+ *
+ * @param unknown_type $hook
+ * @param unknown_type $entity_type
+ * @param unknown_type $returnvalue
+ * @param unknown_type $params
+ */
+function image_captcha_actionlist_hook($hook, $entity_type, $returnvalue, $params){
+	if (!is_array($returnvalue)){
+		$returnvalue = array();
 	}
 		
-	// register default elgg events
-	elgg_register_event_handler("init", "system", "image_captcha_init");
+	$returnvalue[] = 'register';
+	$returnvalue[] = 'user/requestnewpassword';
+		
+	return $returnvalue;
+}
+	
+// register default elgg events
+elgg_register_event_handler("init", "system", "image_captcha_init");
 	
