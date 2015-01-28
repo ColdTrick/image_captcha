36477cf37ed62d93e62e6954f9a4af3022709789
Juho Jaakkola <juho.jaakkola@mediamaisteri.com>
2015-01-28 14:56:22 +0200

    Adds Finnish translations file

---
+++ languages/fi.php
@@ -0,0 +1,29 @@
+<?php
+
+$finnish = array(
+	'image_captcha' => "Kuva-Captcha",
+
+	'image_captcha:settings:icons' => "Valitse käytettävät ikonit",
+	'image_captcha:settings:icons:general' => "Yleiset",
+	'image_captcha:settings:icons:fruit' => "Hedelmät",
+
+	'image_captcha:icon_types:fruit:apple' => "Omena",
+	'image_captcha:icon_types:fruit:cherry' => "Kirsikka",
+	'image_captcha:icon_types:fruit:lemon' => "Sitruuna",
+	'image_captcha:icon_types:fruit:pear' => "Päärynä",
+	'image_captcha:icon_types:fruit:strawberry' => "Mansikka",
+
+	'image_captcha:icon_types:general:folder' => "Kansio",
+	'image_captcha:icon_types:general:house' => "Talo",
+	'image_captcha:icon_types:general:lock' => "Lukko",
+	'image_captcha:icon_types:general:man' => "Mies",
+	'image_captcha:icon_types:general:monitor' => "Näyttö",
+	'image_captcha:icon_types:general:rss' => "RSS-ikoni",
+	'image_captcha:icon_types:general:woman' => "Nainen",
+
+	'image_captcha:label' => "Todista olevasi ihminen klikkaamalla kuvaa, jossa on <u>%s</u>",
+
+	'image_captcha:verify:fail' => "Valitsemasi kuva oli väärin. Yritä uudelleen.",
+);
+
+add_translation("fi", $finnish);
